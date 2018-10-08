7f280ae028ac7b4f7318aba0d4de12498643877e
arynasun <arinateulovich@gmail.com>
2018-10-08 23:58:25 +0300

    validatorTest

--- Task1/src/test/java/validator/ValidatorTest.java
+++ Task1/src/test/java/validator/ValidatorTest.java
@@ -1,41 +1,17 @@
 package validator;
 
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ValidatorTest extends Assert {
 
-    @DataProvider(name = "dataProvider")
-    public Object[][] provide() {
-        List<String> lines = new ArrayList<>();
-        lines.add("1.0 0.2 0.3 0.5");
-        lines.add("-15.0 0.2 0.3 0.5");
-        lines.add("15.0 0.f2 0.3 0.5");
-        lines.add("1.0 0.2 0.3");
-        lines.add("15.0 0.2 0.3 0.9595");
-        List<Boolean> answers = new ArrayList<>();
-        answers.add(true);
-        answers.add(false);
-        answers.add(false);
-        answers.add(false);
-        answers.add(true);
-        return new Object[][]{
-                {lines, answers}
-        };
-    }
+    @Test
+    public void validateTest() {
+        String line = "5.3 2525.2 2.3 2.3";
 
-    @Test(dataProvider = "dataProvider")
-    public void validateTest(List<String> lines, List<Boolean> answers) {
-        for (int i = 0; i < lines.size(); i++) {
-            boolean expected = answers.get(i);
+        boolean actual = Validator.validate(line);
 
-            boolean actual = Validator.validate(lines.get(i));
+        assertTrue(actual);
 
-            assertEquals(expected, actual);
-        }
     }
 }
